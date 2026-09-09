09772f45d3b38f99c0a2b47f18ac41ed76dd639c
Oksana <oksana.krychynska@gmail.com>
2026-09-09 18:17:08 +0300

    minor updates to main

--- src/app/features/books/api/books-api.service.ts
+++ src/app/features/books/api/books-api.service.ts
@@ -11,7 +11,7 @@ export class BooksApiService  {
   private readonly http = inject(HttpClient);
 
   getBooks() {
-    return this.http.get<Book[]>('/books.json').pipe(
+    return this.http.get<Book[]>('./books.json').pipe(
       map(books => books.map((book, index) => ({
           ...book,
           id: index + 1,
