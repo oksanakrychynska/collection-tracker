5359a76aa11f789a0b8b1998e74925702c5b7616
Oksana <oksana.krychynska@gmail.com>
2026-09-13 14:28:35 +0300

    service-worker init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {MAT_ICON_DEFAULT_OPTIONS} from '@angular/material/icon';
 import { routes } from './app.routes';
 import {authInterceptor} from './core/auth/auth.interceptor';
-import firebase from 'firebase/compat/app';
-import initializeApp = firebase.initializeApp;
-import {environment} from '../environments/environment';
+import {provideServiceWorker} from '@angular/service-worker';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,7 +16,13 @@ export const appConfig: ApplicationConfig = {
     {
       provide: MAT_ICON_DEFAULT_OPTIONS,
       useValue: { fontSet: 'material-symbols-outlined' }
-    },
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
 
   ]
 };
